19ca91320cbe482d111fc93a320a6769fda7447b
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-06-22 05:11:46 +0530

    doen embedding and list embedding file to get name of the embedding model from google

---
+++ embeddings.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ( ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings)
+
+load_dotenv()
+
+texts = [
+    "Paris is the capital of France",
+    "Delhi is the capital of India",
+    "Tokyo is the capital of Japan"
+    ]
+
+
+def main():
+
+    client = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+    response = client.invoke("What is the meaning of Gooning")
+
+    print(response.content[:100])
+
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/gemini-embedding-2"
+    )
+
+    vector = embeddings.embed_query(response.content)
+
+    print(f"Vecton Length: {len(vector)}")
+    print(vector[:10]) 
+
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ list_models.py
@@ -0,0 +1,13 @@
+"""
+just for getting the name of the embedding models
+
+"""
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client()
+
+for model in client.models.list():
+    print(model.name)
