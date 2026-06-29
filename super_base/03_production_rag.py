9dbee0c1d6eefea4c64781a3a6dd5a452e6d0abd
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-06-30 03:48:10 +0530

    imported all the necessary imports for production project with superbase pgvector

---
+++ super_base/03_production_rag.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from langchain_postgres import PGVector
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.documents import Document
+
+load_dotenv()
+
+
