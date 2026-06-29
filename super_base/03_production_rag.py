65cca5f74addb536d38eef0e666b19b73efe1209
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-06-30 03:55:11 +0530

    defined model settings

--- super_base/03_production_rag.py
+++ super_base/03_production_rag.py
@@ -5,7 +5,22 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.documents import Document
+import os
 
 load_dotenv()
 
+
+# configration
+@dataclass
+class Config:
+
+    # Database - use pooler Url in production
+    database_url: str = os.getenv("SUPERBASE_DATABASE_URL")
+
+    collection_name: str = "ankit_production_documents"
+
+    # Model settings
+
+    embedding_model: str = "models/gemini-embedding-2"
+    chat_model : str = "gemini-2.5-flash"
 
